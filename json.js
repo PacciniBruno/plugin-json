58caaf7075be4f923e22a9f851ec79fcaf75151e
Bruno <bruno@paccini.com>
2016-04-21 16:06:34 +0200

    get rid of the 'instanciate' hook

--- json.js
+++ json.js
@@ -4,11 +4,13 @@
 
 define({
   translate: function(load) {
-    if (this.builder)
-      return 'export default ' + JSON.stringify(JSON.parse(load.source));
+    if (System.transpiler === false || true) {
+    load.metadata.format = 'amd';
+      return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
+    }
+
+    // disabled pending rollout of https://github.com/systemjs/builder/commit/13ed8f896ab2df733252f75d8ed770c2d9e11862
+    load.metadata.format = 'esm';
+    return 'export default ' + JSON.stringify(JSON.parse(load.source)) + ';';
   },
-  instantiate: function(load) {
-    if (!this.builder)
-      return JSON.parse(load.source);
-  }
 });
