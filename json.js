0335cf2d374fc5656d4d0121776656c49f66c19a
Bruno <bruno@paccini.com>
2016-04-25 14:16:32 +0200

    parse json even in build mode

--- json.js
+++ json.js
@@ -6,7 +6,7 @@ define({
   translate: function(load) {
     if (System.transpiler === false || true) {
     load.metadata.format = 'amd';
-      return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
+      return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(JSON.parse(load.source)) + ';\n});';
     }
 
     // disabled pending rollout of https://github.com/systemjs/builder/commit/13ed8f896ab2df733252f75d8ed770c2d9e11862
